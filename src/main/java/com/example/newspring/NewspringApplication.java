6efbccc59c7839e59ab8d7152d6305a70d50810f
Purushothshanmugam <purushothaman.shanmugam@ideas2it.com>
2023-03-05 23:32:29 +0530

    spring-boot-application

---
+++ src/main/java/com/example/newspring/NewspringApplication.java
@@ -0,0 +1,20 @@
+package com.example.newspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NewspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspringApplication.class, args);
+	}
+	@GetMapping(value = "/spring")
+	public String welcome(){
+		return "hello spring";
+	}
+
+}
